b9ca6bea08ceabcad6a364e5607d25879a4c523e
Rhavgar <53013070+Rhavgar@users.noreply.github.com>
2021-10-16 09:46:38 -0300

    Fixed Main.java

--- src/biblioteca/Main.java
+++ src/biblioteca/Main.java
@@ -5,15 +5,5 @@ public class Main
     public static void main(String args[])
     {
         // para futuro testes
-        System.out.print(calcularFibonacciRecursivo(5));
-    }
-    
-    static long calcularFibonacciRecursivo(int termo) {
-		if(termo <= 2) {
-			return 1;
-		}
-		else {
-			return calcularFibonacciRecursivo(termo-1) + calcularFibonacciRecursivo(termo-2);
-		} 	
     }
 }
